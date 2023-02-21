472c9ba8cd4de3dbd0d834656a0ce16633f786e7
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2023-02-21 15:55:32 +0200

    some changes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from sqlite3 import DatabaseError
+
 from flask import redirect, url_for, render_template, request, session, flash
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 from forms import RegistrationForm, LoginForm, UserForm
@@ -43,17 +45,17 @@ def profile():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email = form.email.data).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is not None and user.check_password(form.password.data):
             session["logged_in"] = True
             login_user(user, remember=form.remember.data)
-            next = request.args.get("next")
-            return redirect(next or url_for('home'))
+            # next = request.args.get("next")
+            return redirect(url_for("profile"))
         flash('Невалиднa парола. Моля, опитайте пак.')
     return render_template('login.html', form=form)
 
 
-@app.route('/register', methods = ['POST','GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -65,34 +67,35 @@ def register():
         elif email:
             flash('Този имейл адрес вече съществува')
             return redirect(url_for('register'))
-        username = User(username=form.username.data, email=form.email.data,
+        new_user = User(username=form.username.data, email=form.email.data,
                         first_name=form.first_name.data, last_name=form.last_name.data)
-        username.set_password(form.password1.data)
-        db.session.add(username)
+        new_user.set_password(form.password1.data)
+        db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('registration.html', form=form)
 
 
 @app.route("/update/<int:id>", methods=["GET", "POST"])
+@login_required
 def update(id):
     form = UserForm()
+    id = current_user.id
     name_to_update = User.query.get_or_404(id)
     if request.method == "POST":
-        name_to_update.first_name = request.form["First Name"]
-        name_to_update.last_name = request.form["Last Name"]
+        name_to_update.first_name = request.form["first_name"]
+        name_to_update.last_name = request.form["last_name"]
         name_to_update.username = request.form["username"]
-        name_to_update.email = request.form["Email"]
-        # name_to_update.password = request.form["Password"]
+        name_to_update.email = request.form["email"]
         try:
             db.session.commit()
             flash("Акаунта е редактиран успешно")
-            return render_template("update.html", form=form, name_to_update=name_to_update)
-        except:
+            return render_template("profile.html", form=form, name_to_update=name_to_update, id=id)
+        except DatabaseError:
             flash("Възникна грешка! Моля опитайте отново!")
-            return render_template("update.html", form=form, name_to_update=name_to_update)
+            return render_template("update.html", form=form, name_to_update=name_to_update, id=id)
     else:
-        return render_template("update.html", form=form, name_to_update=name_to_update)
+        return render_template("update.html", form=form, name_to_update=name_to_update, id=id)
 
 
 @app.route("/logout")
@@ -100,7 +103,7 @@ def logout():
     logout_user()
     session.clear()
     session['_remember'] = 'clear'
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 
 @app.route("/user_view")
--- forms.py
+++ forms.py
@@ -1,30 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    first_name = StringField('First Name')
-    last_name = StringField('Last name')
-    password1 = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password1')])
-    submit = SubmitField('Регистрация')
+    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={"placeholder": "Username"})
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    first_name = StringField("First Name")
+    last_name = StringField("Last name")
+    password1 = PasswordField("Password", validators=[InputRequired(), Length(min=8, max=20)],
+                              render_kw={"placeholder": "Password"})
+    password2 = PasswordField("Confirm Password", validators=[DataRequired(),
+                                                              EqualTo("password1", message="Паролите трябва да съвпадат")])
+    submit = SubmitField("Регистрация")
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Вход')
+    # email = StringField("Email", validators=[DataRequired(), Email()])
+    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Запомни ме")
+    submit = SubmitField("Вход")
 
 
 class UserForm(FlaskForm):
-    username = StringField("Name", validators=[DataRequired()])
+    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={"placeholder": "Username"})
     email = StringField("Email", validators=[DataRequired(), Email()])
-    first_name = StringField('First Name')
-    last_name = StringField('Last name')
-    password1 = PasswordField('Password', )
-    password2 = PasswordField('Confirm Password', [EqualTo('password1')])
-    submit = SubmitField("Submit")
+    first_name = StringField("Име")
+    last_name = StringField("Фамилия")
+    password1 = PasswordField("Password")
+    password2 = PasswordField("Confirm Password", [EqualTo("password1")])
+    submit = SubmitField("Потвърди")
